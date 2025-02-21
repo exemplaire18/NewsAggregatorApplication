88f8c5e921387a8622ab89e2c61b9b9dca44f2ca
Ashwin Dutta <dutta.ashwin18@gmail.com>
2025-02-21 14:43:57 +0530

    refactor: response dto refactoring

--- src/main/java/com/newsaggregatorapplication/dto/ResponseDTO.java
+++ src/main/java/com/newsaggregatorapplication/dto/ResponseDTO.java
@@ -14,23 +14,17 @@ import java.util.Map;
 public class ResponseDTO {
     @Schema(description = "Search keyword used", example = "apple")
     private String searchKeyword;
-    private String city;
+    @Schema(description = "Time taken to fetch the articles based on the keyword")
     private long timeTakenMs;
-
+    @Schema(description = "List of articles (deduplicated)")
+    private List<ArticleDTO> articles;
     @Schema(description = "Current page number", example = "1")
     private int currentPage;
     @Schema(description = "Total pages available", example = "5")
     private int totalPages;
-    private Integer previousPageNo;
-    private Integer nextPageNo;
-
-    @Schema(description = "List of articles (deduplicated)")
-    private List<ArticleDTO> articles;
-
-    // HATEOAS links
+    @Schema(description = "Links for previous/next pages")
     private Map<String, String> links;
 
-    // Helper method to add pagination links
     public void addLink(String rel, String href) {
         if (links == null) links = new HashMap<>();
         links.put(rel, href);
